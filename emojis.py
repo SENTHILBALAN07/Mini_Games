d506c696df1ac3d062cc7c107750f164a892fca4
SENTHILBALAN07 <116082466+SENTHILBALAN07@users.noreply.github.com>
2024-07-31 19:22:56 +0530

    Add files via upload

---
+++ emojis.py
@@ -0,0 +1,15 @@
+def emojiConvertor(message):
+    words = message.split(' ')
+    emojis = {
+        ":)": "😌",
+        ":(":"😔"
+    }
+    output = ""
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output
+
+
+message=input(">")
+print(emojiConvertor(message))
+
